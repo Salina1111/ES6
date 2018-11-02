c74310a965c23bde197e1b70d9c8e7d80401a905
Salina <salina.sallu@outlook.com>
2018-11-02 14:52:57 +0545

    Palindrome

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ function reverseString(str) {
     var a = [...str]//use of spread to insert the given string into an array
     for (let i = str.length - 1, j = 0; i >= 0; i--, j++)
     a[j] = str[i];
-    return a.join('');
+    b = a.join('');
+  
+   if(b===str ){
+        alert(str+" " +"is palindrome");
+        
+    }else{
+        alert(str+" "+"is not palindrome");
+    }
+    return b;
 }
-  console.log(reverseString('Salina'));
+  console.log(reverseString('eye'));
