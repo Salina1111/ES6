a9e54ed87729d4bd2070ce641d4521a004bc7e18
Salina <salina.sallu@outlook.com>
2018-11-02 15:55:38 +0545

    integer reverse using chaining done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Reverse string
+//Reverse string and Palindrome
 function reverseString(str) {
     var a = [...str]//use of spread to insert the given string into an array
     for (let i = str.length - 1, j = 0; i >= 0; i--, j++)
@@ -14,3 +14,11 @@ function reverseString(str) {
     return b;
 }
   console.log(reverseString('eye'));
+
+  //Integer reversal
+function Int(num) {
+    let r = num.toString().split('').reverse().join('')
+    return r;
+}
+
+console.log(Int(12345));
